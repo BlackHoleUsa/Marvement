d585285121be68bbd8a0b493215f3910fd3b5046
shamoon1997 <sshamoon83@gmail.com>
2022-01-19 17:01:00 +0500

    handled github actions

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -82,7 +82,6 @@ const unfollowUser = catchAsync(async (req, res) => {
   const user = req.user;
 
   await userService.unFollowUser(user._id, otherUserId);
-  console.log("is")
   res.status(httpStatus.OK).send({
     status: true,
     message: 'user unfollowed successfully',

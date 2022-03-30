639cdd5f33fbb5b481f47a46cf9b57398f488439
shamoon1997 <sshamoon83@gmail.com>
2022-03-30 14:03:17 +0500

    video server created

--- src/controllers/artwork.controller.js
+++ src/controllers/artwork.controller.js
@@ -29,9 +29,9 @@ const saveArtwork = catchAsync(async (req, res) => {
       thumbNailData = await addFilesToIPFS(files[1].buffer, 'artwork_thumbnail_image');
     }
   }
-  let genre = req.body?.genre;
+  let genre = req.body.genre;
   genre = genre?.split(',');
-  req.body?.genre = genre;
+  req.body.genre = genre;
   // let arr = req.body.genre;
   // console.log(arr);
   // let arr2 = JSON.parse(arr);
